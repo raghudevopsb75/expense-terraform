9d08bd76f88435e22680a5c48de56980125ed9a6
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-09-30 08:41:29 +0530

    Expense Scripts

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,4 +5,25 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
+}
+
+
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  tags = {
+    Name = "private-subnet-${count.index}"
+  }
+}
+
 
--- modules/vpc/vars.tf
+++ modules/vpc/vars.tf
@@ -1,3 +1,7 @@
 variable "vpc_cidr" {}
 variable "env" {}
 
+variable "public_subnets" {}
+variable "private_subnets" {}
+variable "azs" {}
+
