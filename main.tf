357ba89f26b9e3eb8c9096e63def834b8586beb7
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-10-02 09:00:39 +0530

    Expense Scripts

--- main.tf
+++ main.tf
@@ -31,4 +31,13 @@ module "private-lb" {
   vpc_id            = module.vpc.vpc_id
 }
 
+module "frontend" {
+  source = "./modules/app"
+  app_port      = 80
+  component     = "frontend"
+  env           = var.env
+  instance_type = "t3.micro"
+  vpc_cidr      = var.vpc_cidr
+  vpc_id        = module.vpc.vpc_id
+}
 
