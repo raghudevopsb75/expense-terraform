1891b7d9a2655ae8808b1741337a2ac34d88540f
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-10-04 08:34:16 +0530

    Expense Scripts

--- main.tf
+++ main.tf
@@ -63,6 +63,6 @@ module "mysql" {
   env       = var.env
   subnets   = module.vpc.private_subnets
   vpc_cidr  = var.vpc_cidr
-  vpc_id    =  module.vpc.vpc_id
+  vpc_id    = module.vpc.vpc_id
 }
 
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -33,14 +33,14 @@ resource "aws_db_subnet_group" "main" {
 }
 
 resource "aws_rds_cluster" "main" {
-  cluster_identifier      = "${var.env}-${var.component}"
-  engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.11.3"
-  db_subnet_group_name    = aws_db_subnet_group.main.name
-  database_name           = "dummy"
-  master_username         = data.aws_ssm_parameter.master_username.value
-  master_password         = data.aws_ssm_parameter.master_password.value
-  vpc_security_group_ids  = [aws_security_group.main.id]
+  cluster_identifier     = "${var.env}-${var.component}"
+  engine                 = "aurora-mysql"
+  engine_version         = "5.7.mysql_aurora.2.11.3"
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  database_name          = "dummy"
+  master_username        = data.aws_ssm_parameter.master_username.value
+  master_password        = data.aws_ssm_parameter.master_password.value
+  vpc_security_group_ids = [aws_security_group.main.id]
 }
 
 resource "aws_rds_cluster_instance" "main" {
