b4c5371c01821b53d39ea4ceab2ef3e4ef554cbd
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-10-05 08:52:09 +0530

    Expense Scripts

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "security_group" {
     cidr_blocks = [var.alb_sg_allow_cidr]
   }
 
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.alb_sg_allow_cidr]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -46,6 +54,21 @@ resource "aws_lb_listener" "listener-http" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = var.tg_arn
+  }
+}
+
+resource "aws_lb_listener" "listener-https" {
+  count             = var.alb_type == "public" ? 1 : 0
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-east-1:739561048503:certificate/6414e7b8-1da5-4a92-a3a8-d66334ae5696"
+
 
   default_action {
     type             = "forward"
